8b3579c5d3c03cb9a3e1c38ed31baa11996a5464
JaimeHot69 <92993966+JaimeHot69@users.noreply.github.com>
2021-10-22 09:16:28 -0500

    Add files via upload

---
+++ Login/Login.js
@@ -0,0 +1,17 @@
+function Validar() {
+	var U, P;
+	U=document.getElementById("Usu").value;
+	P=document.getElementById("Pass").value;
+	if (U=="Jaime"&&P=="JJRC9") {
+		alert("Tu Usuario y Contraseña son Correctos");
+		window.open("pesogal.html");
+		document.getElementById("Usu").value="";
+		document.getElementById("Pass").value="";
+	}
+	else{
+		alert("Usuario o Contraseña Incorrectos");
+		document.getElementById("Usu").value="";
+		document.getElementById("Pass").value="";
+	}
+
+}
